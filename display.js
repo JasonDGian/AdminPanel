73af8febc0c7f1e7e033290874aab93f4edeebd6
Virgimor <virgi.mor.98@gmail.com>
2023-12-21 12:16:33 +0100

    Modificación del html y creacion de JS

---
+++ display.js
@@ -0,0 +1,161 @@
+function myFunction() {
+    var x = document.getElementById("Shutdown");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    document.getElementById("Block") .style.display = "none";
+    
+    document.getElementById("Screenshot") .style.display = "none";
+
+    document.getElementById("Open") .style.display = "none";
+    document.getElementById("Install") .style.display = "none";
+    document.getElementById("Uninstall") .style.display = "none";
+    document.getElementById("WIFI") .style.display = "none";
+    document.getElementById("Data") .style.display = "none";
+    document.getElementById("Run") .style.display = "none";
+    document.getElementById("Shutdown") .disabled = "true";
+    document.getElementById("Block") .disabled = "false";
+}
+
+  function myFunction1() {
+    var x = document.getElementById("Block");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    document.getElementById("Shutdown") .style.display = "none";
+
+    document.getElementById("Screenshot") .style.display = "none";
+    document.getElementById("Open") .style.display = "none";
+    document.getElementById("Install") .style.display = "none";
+    document.getElementById("Uninstall") .style.display = "none";
+    document.getElementById("WIFI") .style.display = "none";
+    document.getElementById("Data") .style.display = "none";
+    document.getElementById("Run") .style.display = "none";
+    document.getElementById("Block") .disabled = "true";
+    document.getElementById("Shutdown") .disabled = "true";
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("Screenshot");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    document.getElementById("Shutdown") .style.display = "none";
+
+    document.getElementById("Block") .style.display = "none";
+    document.getElementById("Open") .style.display = "none";
+    document.getElementById("Install") .style.display = "none";
+    document.getElementById("Uninstall") .style.display = "none";
+    document.getElementById("WIFI") .style.display = "none";
+    document.getElementById("Data") .style.display = "none";
+    document.getElementById("Run") .style.display = "none";
+  }
+  function myFunction3() {
+    var x = document.getElementById("Open");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    document.getElementById("Shutdown") .style.display = "none";
+
+    document.getElementById("Screenshot") .style.display = "none";
+    document.getElementById("Block") .style.display = "none";
+    document.getElementById("Install") .style.display = "none";
+    document.getElementById("Uninstall") .style.display = "none";
+    document.getElementById("WIFI") .style.display = "none";
+    document.getElementById("Data") .style.display = "none";
+    document.getElementById("Run") .style.display = "none";
+  }
+  function myFunction4() {
+    var x = document.getElementById("Install");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    document.getElementById("Shutdown") .style.display = "none";
+
+    document.getElementById("Screenshot") .style.display = "none";
+    document.getElementById("Open") .style.display = "none";
+    document.getElementById("Block") .style.display = "none";
+    document.getElementById("Uninstall") .style.display = "none";
+    document.getElementById("WIFI") .style.display = "none";
+    document.getElementById("Data") .style.display = "none";
+    document.getElementById("Run") .style.display = "none";
+  }
+  function myFunction5() {
+    var x = document.getElementById("Uninstall");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    document.getElementById("Shutdown") .style.display = "none";
+
+    document.getElementById("Screenshot") .style.display = "none";
+    document.getElementById("Open") .style.display = "none";
+    document.getElementById("Install") .style.display = "none";
+    document.getElementById("Block") .style.display = "none";
+    document.getElementById("WIFI") .style.display = "none";
+    document.getElementById("Data") .style.display = "none";
+    document.getElementById("Run") .style.display = "none";
+  }
+  function myFunction6() {
+    var x = document.getElementById("WIFI");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    document.getElementById("Shutdown") .style.display = "none";
+
+    document.getElementById("Screenshot") .style.display = "none";
+    document.getElementById("Open") .style.display = "none";
+    document.getElementById("Install") .style.display = "none";
+    document.getElementById("Uninstall") .style.display = "none";
+    document.getElementById("Block") .style.display = "none";
+    document.getElementById("Data") .style.display = "none";
+    document.getElementById("Run") .style.display = "none";
+  }
+  function myFunction7() {
+    var x = document.getElementById("Data");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    document.getElementById("Shutdown") .style.display = "none";
+
+    document.getElementById("Screenshot") .style.display = "none";
+    document.getElementById("Open") .style.display = "none";
+    document.getElementById("Install") .style.display = "none";
+    document.getElementById("Uninstall") .style.display = "none";
+    document.getElementById("WIFI") .style.display = "none";
+    document.getElementById("Block") .style.display = "none";
+    document.getElementById("Run") .style.display = "none";
+  }
+  function myFunction8() {
+    var x = document.getElementById("Run");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    document.getElementById("Shutdown") .style.display = "none";
+
+    document.getElementById("Screenshot") .style.display = "none";
+    document.getElementById("Open") .style.display = "none";
+    document.getElementById("Install") .style.display = "none";
+    document.getElementById("Uninstall") .style.display = "none";
+    document.getElementById("WIFI") .style.display = "none";
+    document.getElementById("Data") .style.display = "none";
+    document.getElementById("Block") .style.display = "none";
+  }
+  
